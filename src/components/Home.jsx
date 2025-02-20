27bc285a0c0591d1c415d535b5f6e1d9fdbcaf65
Mahi singh <aka.mahisingh@gmail.com>
2025-02-20 12:33:46 +0530

    .001

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import '../components/Styles/Home.css'
 const Home = () => {
   return (
     <div className="home-container">
-     <h1>Welcome to SkillUp</h1>
+     <h1>Welcome to SkillUp ji</h1>
      
     </div>
   );
