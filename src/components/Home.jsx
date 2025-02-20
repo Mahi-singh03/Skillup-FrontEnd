a4d18ffbff9878de606b79973043b996b5dd4de0
Mahi singh <aka.mahisingh@gmail.com>
2025-02-20 12:32:12 +0530

    New 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import '../components/Styles/Home.css'
 const Home = () => {
   return (
     <div className="home-container">
-      <video src="https://res.cloudinary.com/dufxj1sau/video/upload/v1739961161/xdbwjacgoifdvwfqdkbd.mp4" autoPlay loop muted className="background-video"></video>
+     <h1>Welcome to SkillUp</h1>
      
     </div>
   );
