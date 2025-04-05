f0bf12441cd9470ca64511499ba8ea67dfe25409
Mahi singh <aka.mahisingh@gmail.com>
2025-04-05 10:48:04 +0530

    final edit and branch created

--- src/pages/WeeklyExamList.jsx
+++ src/pages/WeeklyExamList.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom"; // Assuming you are using React Router 
 import "./Styles/WeeklyExanList.css";
 const exams = [
   { id: "Weekly Exam 7 ( 05-03-2025 )", title: "Weekly Exam 7 ( 05-04-2025 )", path: "/Exams/Weekly-Exam/WE_7" },
-  { id: "Weekly Exam 6 ( 29-03-2025 )", title: "Weekly Exam 6 ( 23-03-2025 )", path: "/Exams/Weekly-Exam/WE_6" },
+  { id: "Weekly Exam 6 ( 29-03-2025 )", title: "Weekly Exam 6 ( 29-03-2025 )", path: "/Exams/Weekly-Exam/WE_6" },
   { id: "Weekly Exam 5 ( 22-03-2025 )", title: "Weekly Exam 5 ( 22-03-2025 )", path: "/Exams/Weekly-Exam/WE_5" },
   { id: "Weekly Exam 4 ( 15-03-2025 )", title: "Weekly Exam 4 ( 15-03-2025 )", path: "/Exams/Weekly-Exam/WE_4" },
   { id: "Weekly Exam 3 ( 08-03-2025 )", title: "Weekly Exam 3 ( 08-03-2025 )", path: "/Exams/Weekly-Exam/WE_3" },
   { id: "Weekly Exam 2 ( 01-03-2025 )", title: "Weekly Exam 2 ( 01-03-2025 )", path: "/Exams/Weekly-Exam/WE_2" },
-  { id: "Weekly Exam 1 ( 22-02-2025 )", title: "Weekly Exam 1 ( 22-02-2025 ) ",path: "/Exams/Weekly-Exam/WE_1" },
+  { id: "Weekly Exam 1 ( 22-02-2025 )", title: "Weekly Exam 1 ( 22-02-2025 )", path: "/Exams/Weekly-Exam/WE_1" },
 
 ];
 
-const ExamList = () => {
+const ExamList = () => { 
   return (
     <div className="exam-list-container">
       <h1 className="main-heading">Available Exams</h1>
